fd77a55d52313ac331698c7f4a03c29aa6da35e8
Piotr Przeliorz <piotr@swishlabs.com>
2019-04-07 20:41:31 +0200

    Initial Commit

---
+++ algorithms+data-structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  algorithms+data-structures
+//
+//  Created by Piotr Przeliorz on 07/04/2019.
+//  Copyright © 2019 Piotr Przeliorz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
